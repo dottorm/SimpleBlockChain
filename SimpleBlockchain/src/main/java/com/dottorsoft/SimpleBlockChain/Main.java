9e3c0e351dfb8f7bb02a40d43976b88eaf8523af
marco <marco@192.168.106.97>
2018-02-07 14:06:36 +0100

    Blocks and SHA256

--- SimpleBlockchain/src/main/java/com/dottorsoft/SimpleBlockChain/Main.java
+++ SimpleBlockchain/src/main/java/com/dottorsoft/SimpleBlockChain/Main.java
@@ -1,10 +1,19 @@
 package com.dottorsoft.SimpleBlockChain;
 
+import com.dottorsoft.SimpleBlockChain.core.Block;
 
 public class Main 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) {
+		
+		Block genesisBlock = new Block("Hi im the first block", "0");
+		System.out.println("Hash for block 1 : " + genesisBlock.hash);
+		
+		Block secondBlock = new Block("Yo im the second block",genesisBlock.hash);
+		System.out.println("Hash for block 2 : " + secondBlock.hash);
+		
+		Block thirdBlock = new Block("Hey im the third block",secondBlock.hash);
+		System.out.println("Hash for block 3 : " + thirdBlock.hash);
+		
+	}
 }
---
+++ SimpleBlockchain/src/main/java/com/dottorsoft/SimpleBlockChain/core/Block.java
@@ -0,0 +1,30 @@
+package com.dottorsoft.SimpleBlockChain.core;
+
+import java.util.Date;
+
+import com.dottorsoft.SimpleBlockChain.utils.StringUtil;
+
+public class Block {
+
+	public String hash;
+	public String previousHash;
+	private String data; //a simple message.
+	private long timeStamp;
+
+	//Block Constructor.
+	public Block(String data,String previousHash ) {
+		this.data = data;
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+		this.hash = calculateHash();
+	}
+	
+	public String calculateHash() {
+		String calculatedhash = StringUtil.applySha256( 
+				previousHash +
+				Long.toString(timeStamp) +
+				data 
+				);
+		return calculatedhash;
+	}
+}
