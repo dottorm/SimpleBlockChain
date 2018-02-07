67136a6e2634dd1267d9bae21d502858f1bb9050
marco <marco@192.168.106.97>
2018-02-07 17:13:08 +0100

    working solution

--- SimpleBlockchain/src/main/java/com/dottorsoft/SimpleBlockChain/core/Block.java
+++ SimpleBlockchain/src/main/java/com/dottorsoft/SimpleBlockChain/core/Block.java
@@ -1,5 +1,6 @@
 package com.dottorsoft.SimpleBlockChain.core;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import com.dottorsoft.SimpleBlockChain.utils.StringUtil;
@@ -7,43 +8,55 @@ import com.dottorsoft.SimpleBlockChain.utils.StringUtil;
 public class Block {
 
 	public String hash;
-	public String previousHash;
-	private String data; //a simple message.
-	private long timeStamp;
+	public String previousHash; 
+	public String merkleRoot;
+	public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data will be a simple message.
+	public long timeStamp; //as number of milliseconds since 1/1/1970.
+	public int nonce;
 	
-	private int nonce;
-
-	//Block Constructor.
-	public Block(String data,String previousHash ) {
-		this.data = data;
+	//Block Constructor.  
+	public Block(String previousHash ) {
 		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
-		this.hash = calculateHash();
+		
+		this.hash = calculateHash(); //Making sure we do this after we set the other values.
 	}
 	
+	//Calculate new hash based on blocks contents
 	public String calculateHash() {
 		String calculatedhash = StringUtil.applySha256( 
 				previousHash +
 				Long.toString(timeStamp) +
-				data 
+				Integer.toString(nonce) + 
+				merkleRoot
 				);
 		return calculatedhash;
 	}
 	
+	//Increases nonce value until hash target is reached.
 	public void mineBlock(int difficulty) {
-		String target = new String(new char[difficulty]); //Create a string with difficulty * "0" 
+		merkleRoot = StringUtil.getMerkleRoot(transactions);
+		String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0" 
 		while(!hash.substring( 0, difficulty).equals(target)) {
 			nonce ++;
 			hash = calculateHash();
 		}
 		System.out.println("Block Mined!!! : " + hash);
 	}
-
-	public String getData() {
-		return data;
+	
+	//Add transactions to this block
+	public boolean addTransaction(Transaction transaction) {
+		//process transaction and check if valid, unless block is genesis block then ignore.
+		if(transaction == null) return false;		
+		if((previousHash != "0")) {
+			if((transaction.processTransaction() != true)) {
+				System.out.println("Transaction failed to process. Discarded.");
+				return false;
+			}
+		}
+		transactions.add(transaction);
+		System.out.println("Transaction Successfully added to Block");
+		return true;
 	}
-
-	public void setData(String data) {
-		this.data = data;
-	}
+	
 }
--- SimpleBlockchain/src/main/java/com/dottorsoft/SimpleBlockChain/utils/StringUtil.java
+++ SimpleBlockchain/src/main/java/com/dottorsoft/SimpleBlockChain/utils/StringUtil.java
@@ -1,14 +1,27 @@
 package com.dottorsoft.SimpleBlockChain.utils;
 
+import java.security.Key;
 import java.security.MessageDigest;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.util.ArrayList;
+import java.util.Base64;
+
+import com.dottorsoft.SimpleBlockChain.core.Transaction;
+import com.google.gson.GsonBuilder;
 
 public class StringUtil {
+	
 	//Applies Sha256 to a string and returns the result. 
-	public static String applySha256(String input){		
+	public static String applySha256(String input){
+		
 		try {
-			MessageDigest digest = MessageDigest.getInstance("SHA-256");	        
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+	        
 			//Applies sha256 to our input, 
-			byte[] hash = digest.digest(input.getBytes("UTF-8"));	        
+			byte[] hash = digest.digest(input.getBytes("UTF-8"));
+	        
 			StringBuffer hexString = new StringBuffer(); // This will contain hash as hexidecimal
 			for (int i = 0; i < hash.length; i++) {
 				String hex = Integer.toHexString(0xff & hash[i]);
@@ -20,5 +33,70 @@ public class StringUtil {
 		catch(Exception e) {
 			throw new RuntimeException(e);
 		}
-	}	
+	}
+	
+	//Applies ECDSA Signature and returns the result ( as bytes ).
+	public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
+		Signature dsa;
+		byte[] output = new byte[0];
+		try {
+			dsa = Signature.getInstance("ECDSA", "BC");
+			dsa.initSign(privateKey);
+			byte[] strByte = input.getBytes();
+			dsa.update(strByte);
+			byte[] realSig = dsa.sign();
+			output = realSig;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return output;
+	}
+	
+	//Verifies a String signature 
+	public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature) {
+		try {
+			Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+			ecdsaVerify.initVerify(publicKey);
+			ecdsaVerify.update(data.getBytes());
+			return ecdsaVerify.verify(signature);
+		}catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	//Short hand helper to turn Object into a json string
+	public static String getJson(Object o) {
+		return new GsonBuilder().setPrettyPrinting().create().toJson(o);
+	}
+	
+	//Returns difficulty string target, to compare to hash. eg difficulty of 5 will return "00000"  
+	public static String getDificultyString(int difficulty) {
+		return new String(new char[difficulty]).replace('\0', '0');
+	}
+	
+	public static String getStringFromKey(Key key) {
+		return Base64.getEncoder().encodeToString(key.getEncoded());
+	}
+	
+	public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+		int count = transactions.size();
+		
+		ArrayList<String> previousTreeLayer = new ArrayList<String>();
+		for(Transaction transaction : transactions) {
+			previousTreeLayer.add(transaction.transactionId);
+		}
+		ArrayList<String> treeLayer = previousTreeLayer;
+		
+		while(count > 1) {
+			treeLayer = new ArrayList<String>();
+			for(int i=1; i < previousTreeLayer.size(); i++) {
+				treeLayer.add(applySha256(previousTreeLayer.get(i-1) + previousTreeLayer.get(i)));
+			}
+			count = treeLayer.size();
+			previousTreeLayer = treeLayer;
+		}
+		
+		String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+		return merkleRoot;
+	}
 }
