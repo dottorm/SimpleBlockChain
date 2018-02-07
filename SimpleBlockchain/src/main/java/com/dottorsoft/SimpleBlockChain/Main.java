8751dc22e7e2d8c042c3ba2cf5906dd60214570e
mparrillo2 <mparrillo2@192.168.106.97>
2018-02-07 14:17:47 +0100

    GSon instroduction

--- SimpleBlockchain/src/main/java/com/dottorsoft/SimpleBlockChain/Main.java
+++ SimpleBlockchain/src/main/java/com/dottorsoft/SimpleBlockChain/Main.java
@@ -1,19 +1,23 @@
 package com.dottorsoft.SimpleBlockChain;
 
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+
 import com.dottorsoft.SimpleBlockChain.core.Block;
 
-public class Main 
-{
+public class Main {
+	
+	public static ArrayList<Block> blockchain = new ArrayList<Block>(); 
+	
 	public static void main(String[] args) {
 		
-		Block genesisBlock = new Block("Hi im the first block", "0");
-		System.out.println("Hash for block 1 : " + genesisBlock.hash);
+		blockchain.add(new Block("Hi im the first block", "0"));		
+		blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash)); 
+		blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
 		
-		Block secondBlock = new Block("Yo im the second block",genesisBlock.hash);
-		System.out.println("Hash for block 2 : " + secondBlock.hash);
-		
-		Block thirdBlock = new Block("Hey im the third block",secondBlock.hash);
-		System.out.println("Hash for block 3 : " + thirdBlock.hash);
+		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);		
+		System.out.println(blockchainJson);
 		
 	}
 }
